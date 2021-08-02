645252f1109ee04a53ec1e0a4942a1a50d4cf8bc
ishwarmore21 <ishwar_more@persistent.com>
2021-08-02 14:50:39 +0530

    Customer Table added

--- src/pages/AdminPages/AdminServiceProviders.jsx
+++ src/pages/AdminPages/AdminServiceProviders.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 import './admin-pages.css'
 import Footer from '../../Components/Page2/Footer'
-//import SideBar from '../../Components/Admin/Sidebar/sidebar';
+import ServiceFilter from '../../Components/Admin/ServiceFilter/ServiceFilter'
 
 
 export default class ServiceProviders extends Component {
     render() {
         return (
             <div>
-                {/* <SideBar/> */}
                 <h1 className='title'>ServiceProviders</h1>
                 <h2>Accept Deny Service providers</h2>
+                <div className='accept-deny'></div>
+                <b>Search for a Service Provider</b>
+                <ServiceFilter/>
                 <Footer/>
             </div>
         )
